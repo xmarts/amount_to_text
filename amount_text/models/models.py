d3dcec8bc0d8b7207667cb0691344f3e354cbcfc
javierhilarioxmarts <javier.hilario.xmarts@gmail.com>
2022-04-21 11:34:13 -0500

    [REF] Migrate to version 15.0

--- amount_text/models/models.py
+++ amount_text/models/models.py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*-
-
 from odoo import models, fields, api, _
 from . import amount_to_text
 
+
 class AddFieldAmountText(models.Model):
-	_inherit = "account.move"
+    _inherit = "account.move"
 
-	amount_to_text = fields.Char(compute='_get_amount_to_text', string='Monto en Texto', readonly=True, help='Amount of the invoice in letter')
+    amount_to_text = fields.Char(compute='_get_amount_to_text', string='Monto en Texto', readonly=True, help='Amount of the invoice in letter')
 
-	
-	@api.depends('amount_total')
-	def _get_amount_to_text(self):
-		self.amount_to_text = amount_to_text.get_amount_to_text(self, self.amount_total, self.currency_id.name)
+    @api.depends('amount_total')
+    def _get_amount_to_text(self):
+        self.amount_to_text = amount_to_text.get_amount_to_text(self, self.amount_total, self.currency_id.name)
